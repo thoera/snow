d267dd1664f3f4763352364ca93019f44cb44be8
thoera <erard.thomas@gmail.com>
2017-12-19 21:58:48 +0100

    add a tree

--- snow.R
+++ snow.R
@@ -36,8 +36,37 @@ create_snow <- function(snowflake = 100, length = 100, speed = 0.025) {
               "snow_on_the_ground" = snow_on_the_ground))
 }
 
+tree <- function(center = 0.7) {
+  x <- c(0.23, 0.21, 0.18, 0.14, 0.17, 0.15,
+         0.12, 0.11, 0.13, 0.09, 0.06, 0.03, 0.01)
+  x1 <- center - x / 2
+  x2 <- center + x / 2
+  y <- seq(0.075, 0.025 * (length(x) + 2), 0.025)
+  
+  needles <- data_frame(
+    x1 = x1,
+    y1 = y,
+    x2 = x2,
+    y2 = y
+  )
+  
+  trunk <- data_frame(
+    x1 = center,
+    y1 = 0,
+    x2 = center,
+    y2 = 0.1
+  )
+  return(list("needles" = needles, "trunk" = trunk))
+}
+
 plot_snow <- function(snow_in_the_sky, snow_on_the_ground) {
   p <- ggplot(data = snow_in_the_sky, aes(x = x, y = y, size = size)) +
+    geom_segment(data = christmas_tree[["trunk"]],
+                 aes(x = x1, y = y1, xend = x2, yend = y2),
+                 size = 14, color = "#5e3a12") +
+    geom_segment(data = christmas_tree[["needles"]],
+                 aes(x = x1, y = y1, xend = x2, yend = y2),
+                 size = 7, color = "#006300") +
     geom_point(color = "white", pch = 42, alpha = 0.75) +
     geom_point(data = snow_on_the_ground, aes(x = x, y = y, size = size),
                color = "white", pch = 45, alpha = 0.75) +
@@ -49,7 +78,8 @@ plot_snow <- function(snow_in_the_sky, snow_on_the_ground) {
   print(p)
 }
 
-snow <- create_snow(snowflake = 200, length = 500, speed = 0.005)
+snow <- create_snow(snowflake = 250, length = 500, speed = 0.005)
+christmas_tree <- tree(center = 0.7)
 
 img <- image_graph(width = 1920, height = 1080, res = 96)
 snowstorm <- map(seq_along(snow[[1]]), function(x) {
